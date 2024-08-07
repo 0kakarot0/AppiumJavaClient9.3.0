104003b7fd5b5889b952443b4ba981616b598ba3
Ahtisham <ilyasahtsham@gmail.com>
2024-08-08 02:56:00 +0500

    modified it

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,14 +1,14 @@
 package pages;
 
+import base_page.BasePage;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 import utilities.LocatorStore;
 import utilities.NativeIOSButons;
 import utilities.SeleniumUtils;
 
-public class LoginPage extends SeleniumUtils {
-    private final static String SCREEN_NAME = "loginScreen";
-    private final String platformName;
+public class LoginPage extends BasePage {
+    private static final String SCREEN_NAME = "loginScreen";
 
     By emailAddress;
     By initialPasswordLocator;
@@ -16,30 +16,30 @@ public class LoginPage extends SeleniumUtils {
     By loginButton;
     By notification;
 
-    NativeIOSButons nativeIOSButons;
-    LocatorStore locatorStore;
-
     public LoginPage(AppiumDriver driver, String platformName) {
-        super(driver);
-        this.platformName = platformName;
-        nativeIOSButons = new NativeIOSButons(driver);
-        locatorStore = new LocatorStore();
+        super(driver, platformName);
+        initializeLocators();
+    }
+
+    private void initializeLocators() {
+        emailAddress = getLocator(SCREEN_NAME, "emailField", LocatorStore.LocatorType.XPATH);
+        initialPasswordLocator = getLocator(SCREEN_NAME, "initialPasswordLocator", LocatorStore.LocatorType.XPATH);
+        finalPasswordLocator = getLocator(SCREEN_NAME, "finalPasswordLocator", LocatorStore.LocatorType.XPATH);
+        loginButton = getLocator(SCREEN_NAME, "loginButton", LocatorStore.LocatorType.XPATH);
+        notification = getLocator(SCREEN_NAME, "errorNotification", LocatorStore.LocatorType.XPATH);
     }
 
 
     public void enterEmailAddress(String emailAddressString) {
-        emailAddress = By.xpath(locatorStore.getLocator(platformName, SCREEN_NAME, "emailField", LocatorStore.LocatorType.XPATH));
         clickOnElement(emailAddress);
         enterTextInTextField(emailAddress, emailAddressString);
         doneOrNavigateBack();
     }
 
     public void enterPassword(String passwordString) {
-        initialPasswordLocator = By.xpath(locatorStore.getLocator(platformName, SCREEN_NAME, "initialPasswordLocator", LocatorStore.LocatorType.XPATH));
         clickOnElement(initialPasswordLocator);
 
-        if (platformName.equalsIgnoreCase("ios")) {
-            finalPasswordLocator = By.xpath(locatorStore.getLocator(platformName, SCREEN_NAME, "finalPasswordLocator", LocatorStore.LocatorType.XPATH));
+        if (isIOS()) {
             clickOnElement(finalPasswordLocator);
             enterTextInTextField(finalPasswordLocator, passwordString);
         } else {
@@ -49,20 +49,11 @@ public class LoginPage extends SeleniumUtils {
     }
 
     public void clickLoginButton() {
-        loginButton = By.xpath(locatorStore.getLocator(platformName, SCREEN_NAME, "loginButton", LocatorStore.LocatorType.XPATH));
         clickOnElement(loginButton);
     }
 
     public String getNotificationText() {
-        notification = By.xpath(locatorStore.getLocator(platformName, SCREEN_NAME, "errorNotification", LocatorStore.LocatorType.XPATH));
-        return platformName.equalsIgnoreCase("ios") ? getTextFromElement(notification) : getContentDescription(notification);
+        return isIOS() ? getTextFromElement(notification) : getContentDescription(notification);
     }
 
-    private void doneOrNavigateBack() {
-        if (platformName.equalsIgnoreCase("ios")) {
-            nativeIOSButons.clickOnDoneButton();
-        } else {
-            navigateBack();
-        }
-    }
 }
