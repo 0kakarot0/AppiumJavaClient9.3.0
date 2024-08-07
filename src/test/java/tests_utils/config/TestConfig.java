8b4610a163ad455b6990833ffcfbb2f3d0c349ba
Ahtisham <ilyasahtsham@gmail.com>
2024-08-08 02:57:09 +0500

    added test steps enum

---
+++ src/test/java/tests_utils/config/TestConfig.java
@@ -0,0 +1,9 @@
+package tests_utils.config;
+
+public class TestConfig {
+    public static String LOGIN_NUMBER;
+    public static String ID_NUMBER;
+    public static String DAY_OF_DOB;
+    public static String MONTH_OF_DOB;
+    public static String YEAR_OF_DOB;
+}
