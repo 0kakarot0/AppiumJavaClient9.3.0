311365a14b17f090eee17b943190bc5c8f18c019
Ahtisham <ilyasahtsham@gmail.com>
2024-08-08 02:56:22 +0500

    optimization

--- src/test/java/tests_base/BaseTest.java
+++ src/test/java/tests_base/BaseTest.java
@@ -6,7 +6,9 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import tests_utils.reporter.MyExtentReport;
 
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
@@ -16,14 +18,17 @@ public class BaseTest {
     protected AppiumDriver driver;
     protected static String platformName;
     protected static String deviceName;
+    protected static MyExtentReport extentReport;
 
     @BeforeTest
     public void setUp(ITestContext context) throws MalformedURLException, FileNotFoundException {
         URL url = new URL("http://127.0.0.1:4723/");
         platformName = context.getCurrentXmlTest().getParameter("platform");
         deviceName = context.getCurrentXmlTest().getParameter("deviceName");
-        driver = new AppiumDriver(url, DeviceCapabilitiesManager.getDeviceCapabilitiesOptions(platformName,deviceName));
+        driver = new AppiumDriver(url, DeviceCapabilitiesManager.getDeviceCapabilitiesOptions(platformName, deviceName));
+        extentReport = new MyExtentReport();
     }
+
 
 
     @AfterTest
